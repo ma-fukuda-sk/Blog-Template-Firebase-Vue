e2f876e6fd77871600956dbb13e63530c8ff2241
ma-fukuda <ma-fukuda@skylark.co.jp>
2022-06-25 16:31:55 +0900

    1st

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,7 +1,13 @@
 import * as firebase from "firebase/app";
 
 var config = {
-  FIREBASE_CONFIG_HERE
+  apiKey: "AIzaSyD_6HmDxwolxaUrMNZD2if5fR0R7x1lXCQ",
+  authDomain: "firebolgs.firebaseapp.com",
+  projectId: "firebolgs",
+  storageBucket: "firebolgs.appspot.com",
+  messagingSenderId: "893160595172",
+  appId: "1:893160595172:web:3f61f53c41346d684456f1",
+  measurementId: "G-70PSS276CB"
 };
 
 firebase.initializeApp(config);
